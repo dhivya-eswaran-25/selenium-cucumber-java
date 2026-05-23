db4330e53f5398edaeb8572eac23a2b867c0abbb
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-05-23 21:36:48 +0530

    fixed chromeoptons

--- src/test/java/stepdefs/ChooseBrowser.java
+++ src/test/java/stepdefs/ChooseBrowser.java
@@ -10,7 +10,14 @@ public class ChooseBrowser{
     public static WebDriver chooseBrowser(String browser){
         switch(browser.toLowerCase()){
             case "chrome":
-                return new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless=new"); // Required for CI
+                options.addArguments("--no-sandbox"); // Required for GitHub Actions
+                options.addArguments("--disable-dev-shm-usage"); // Prevents crashes in Docker
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+                //WebDriver driver = new ChromeDriver(options);
+                return new ChromeDriver(options);
             case "edge":
                 return new EdgeDriver();
             case "firefox":
