8405c6f491c04aa4d2bf9d4d7aea56a0abfb84db
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-05-23 21:41:14 +0530

    fix chrome

--- src/test/java/stepdefs/ChooseBrowser.java
+++ src/test/java/stepdefs/ChooseBrowser.java
@@ -1,7 +1,7 @@
 package stepdefs;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
