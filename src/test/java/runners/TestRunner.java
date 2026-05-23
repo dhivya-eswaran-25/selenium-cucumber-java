82af787437680800b8055f1f1798ee20ee5a656e
dhivya-eswaran-25 <e.dhivya74@gmail.com>
2026-05-23 21:01:45 +0530

    updated runner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,10 +1,10 @@
 package runners;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
 @CucumberOptions(
     features="src/test/resources/features",
     dryRun=false,
@@ -13,7 +13,7 @@ import io.cucumber.junit.CucumberOptions;
     tags="@UAT"
     
 )
-public class TestRunner{
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
 
